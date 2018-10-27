5151ddd5b767dd77eabba584a04329a5304491ed
zhuo <510348414@qq.com>
2018-10-27 21:48:14 +0800

    去除eslint

--- server/router.js
+++ server/router.js
@@ -1,38 +1,36 @@
-const url = require('url');
+const url = require('url')
 
 const rules = [
-    {
-        pattern:/^\/$/,
-        action:'static'
-    },
-    {
-        pattern:/^\/.+\.(html|js|css|png|jpg|eot|svg|ttf|woff|woff2|otf)$/,
-        action:'static'
-    },
-    {
-        pattern:/^\/api\//,
-        action:'api'
-    }
+  {
+    pattern: /^\/$/,
+    action: 'static'
+  },
+  {
+    pattern: /^\/.+\.(html|js|css|png|jpg|eot|svg|ttf|woff|woff2|otf)$/,
+    action: 'static'
+  },
+  {
+    pattern: /^\/api\//,
+    action: 'api'
+  }
 ]
 
-exports.router = function(req,res){
+exports.router = function (req, res) {
+  let urlObj = url.parse(req.url)
+  let pathname = urlObj.pathname
+  let action
 
-    let urlObj = url.parse(req.url);
-    let pathname = urlObj.pathname;
-    let action;
-
-    rules.forEach(rule=>{
-        if(rule.pattern.test(pathname)){
-            action = rule.action;
-        }
-    })
-
-    if(action){
-        require('./action/'+action)(req,res)
-    }else{
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end();
+  rules.forEach(rule => {
+    if (rule.pattern.test(pathname)) {
+      action = rule.action
     }
+  })
 
+  if (action) {
+    require('./action/' + action)(req, res)
+  } else {
+    res.statusCode = 404
+    res.setHeader('Content-Type', 'text/plain')
+    res.end()
+  }
 }
