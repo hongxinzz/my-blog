ac6001db4061c34fc483c1a45580ca340bab4ba4
zhuo <510348414@qq.com>
2018-11-07 11:51:15 +0800

    修改markdown转换成html 分别截取标题 标签 内容 时间 图片 等

--- server/action/markHtml.js
+++ server/action/markHtml.js
@@ -12,24 +12,44 @@ const dir = path.resolve(__dirname, '../../markdown')
 
 const filelist = fs.readdirSync(dir) || []
 
-filelist.forEach(fileName => {
-  let fileContent = new String(fs.readFileSync(path.join(dir, fileName)))
-  let title = fileContent.substring(0, fileContent.search(/\r?\n/)).replace(/^#*\s*/, '')
-  let content = marked(fileContent.toString())
-  let dateStr = fileName.substring(0, fileName.indexOf('.'))
-
-  let person = new PostArticle({
-    title: title,
-    content: content,
-    time: moment(dateStr, 'YYYYMMDDhhmm').toDate()
-  })
-  console.log(dateStr)
-  person.save(function (err, comment) {
-    console.log(1, comment)
-    if (comment) {
-      console.log(`${fileName}入库成功`)
-    } else {
-      console.log(err)
-    }
-  })
+PostArticle.remove({},function (err,comment) {
+  if (comment) {
+    console.log('清空上一次数据,开始读取文件')
+    getNewArticle()
+  } else {
+    console.log(err)
+  }
 })
+
+function getNewArticle(){
+  filelist.forEach(fileName => {
+    let fileContent = new String(fs.readFileSync(path.join(dir, fileName)))
+    let title = fileContent.substring(fileContent.search(/\n##title/), fileContent.search(/-title/)).replace(/##title:/, '')
+    let tags = fileContent.substring(fileContent.search(/\n##tags/), fileContent.search(/-tags/)).replace(/##tags:/, '')
+    let cover = fileContent.substring(fileContent.search(/\n##cover/), fileContent.search(/-cover/)).replace(/##cover:/, '')
+    let introduction = fileContent.substring(fileContent.search(/\n##introduction/), fileContent.search(/-introduction/)).replace(/##introduction:/, '')
+    let content = marked(fileContent.substring(fileContent.search(/\n##content/), fileContent.search(/-content/)).replace(/##content:/, ''))
+    let time = fileContent.substring(fileContent.search(/\n##time/), fileContent.search(/-time/)).replace(/##time:/, '')
+
+    let person = new PostArticle({
+      title: title,
+      tags: tags,
+      cover: cover,
+      introduction: introduction,
+      content: content,
+      time: time
+    })
+    // console.log(fileContent.substring(fileContent.search(/\r?\n/)), fileContent.search(/tags/))
+    console.log(time)
+    // console.log(fileContent)
+    person.save(function (err, comment) {
+      // console.log(1, comment)
+      if (comment) {
+        console.log(`${fileName}入库成功`)
+      } else {
+        console.log(err)
+      }
+    })
+  })
+
+}
