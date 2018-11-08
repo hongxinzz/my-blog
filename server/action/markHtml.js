770c1034674f5b11c90998485596d72604ba3bc6
zhuo <510348414@qq.com>
2018-11-08 20:47:23 +0800

    优化数据转换

--- server/action/markHtml.js
+++ server/action/markHtml.js
@@ -12,7 +12,7 @@ const dir = path.resolve(__dirname, '../../markdown')
 
 const filelist = fs.readdirSync(dir) || []
 
-PostArticle.remove({},function (err,comment) {
+PostArticle.remove({}, function (err, comment) {
   if (comment) {
     console.log('清空上一次数据,开始读取文件')
     getNewArticle()
@@ -21,15 +21,17 @@ PostArticle.remove({},function (err,comment) {
   }
 })
 
-function getNewArticle(){
+function getNewArticle () {
   filelist.forEach(fileName => {
     let fileContent = new String(fs.readFileSync(path.join(dir, fileName)))
-    let title = fileContent.substring(fileContent.search(/\n##title/), fileContent.search(/-title/)).replace(/##title:/, '')
-    let tags = fileContent.substring(fileContent.search(/\n##tags/), fileContent.search(/-tags/)).replace(/##tags:/, '')
-    let cover = fileContent.substring(fileContent.search(/\n##cover/), fileContent.search(/-cover/)).replace(/##cover:/, '')
-    let introduction = fileContent.substring(fileContent.search(/\n##introduction/), fileContent.search(/-introduction/)).replace(/##introduction:/, '')
-    let content = marked(fileContent.substring(fileContent.search(/\n##content/), fileContent.search(/-content/)).replace(/##content:/, ''))
-    let time = fileContent.substring(fileContent.search(/\n##time/), fileContent.search(/-time/)).replace(/##time:/, '')
+    let article = fileContent.replace(/[\r\n]/g, '')
+    let title = article.substring(article.search(/##title/), article.search(/-title/)).replace(/##title:/, '')
+    console.log(article)
+    let tags = article.substring(article.search(/##tags/), article.search(/-tags/)).replace(/##tags:/, '')
+    let cover = article.substring(article.search(/##cover/), article.search(/-cover/)).replace(/##cover:/, '')
+    let introduction = article.substring(article.search(/##introduction/), article.search(/-introduction/)).replace(/##introduction:/, '')
+    let content =  marked(fileContent.substring(fileContent.search(/\n##content/), fileContent.search(/-content/)).replace(/##content:/, ''))
+    let time = article.substring(article.search(/##time/), article.search(/-time/)).replace(/##time:/, '')
 
     let person = new PostArticle({
       title: title,
@@ -51,5 +53,4 @@ function getNewArticle(){
       }
     })
   })
-
 }
