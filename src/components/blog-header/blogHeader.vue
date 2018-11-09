74c94d5f071eaf856ab1095174fe76f4f2a94f19
zhuo <510348414@qq.com>
2018-11-09 15:26:26 +0800

    返回顶部

--- src/components/blog-header/blogHeader.vue
+++ src/components/blog-header/blogHeader.vue
@@ -1,5 +1,6 @@
+<!--suppress ALL -->
 <template>
-  <div class="header">
+  <div  :class="searchBarFixed === true ? 'header header-fix' :'header'" id="header">
     <div class="logo"></div>
     <nav class="r-nav">
       <ul>
@@ -7,12 +8,41 @@
         <li><router-link to="/tags">TAGS</router-link></li>
       </ul>
     </nav>
+    <div class="claerfix"></div>
+    <div class="gotop" v-if="searchBarFixed" @click="goToTop()">{{goTopNum}}%</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'blogHeader'
+  name: 'blogHeader',
+  data(){
+    return{
+      searchBarFixed:false,
+      nowScroll:0,
+      goTopNum:''
+    }
+  },
+  created: function () {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    handleScroll () {
+      let dHeight = document.documentElement.scrollHeight
+      let clientH = window.innerHeight || document.documentElement.clientHeight
+      let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+      if (scrollTop > 0) {
+        let validH = dHeight - clientH
+        this.goTopNum =  Math.floor(scrollTop / validH * 100)
+        this.searchBarFixed = true
+      } else {
+        this.searchBarFixed = false
+      }
+    },
+    goToTop(){
+      document.documentElement.scrollTop = 0
+    }
+  }
 }
 </script>
 
@@ -22,10 +52,10 @@ export default {
     top: 0;
     left: 0;
     width: 100%;
-    height: 26px;
+    /*height: 26px;*/
     padding: 16px 0;
     background-color: transparent;
-    transition: all .6s;
+    transition: all 1s;
     z-index: 1000;
     .r-nav{
       float: right;
@@ -46,6 +76,34 @@ export default {
           &:hover{
             opacity: 1;
           }
+        }
+      }
+    }
+    .claerfix{
+      clear: both;
+    }
+    .gotop{
+      position: fixed;
+      right: 30px;
+      bottom: 30px;
+      width: 56px;
+      height: 24px;
+      background: #222;
+      color: #fff;
+      line-height: 24px;
+      text-align: center;
+      cursor: pointer;
+      font-size: 14px;
+      z-index: 10000;
+    }
+  }
+  .header-fix{
+    background: #fff;
+    border: 1px solid #e7eaf1;
+    .r-nav{
+      li{
+        a{
+          color: rgb(102, 102, 102);
         }
       }
     }
