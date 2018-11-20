9355b4e63cf5bc679ebb8ee955d7168674e9c252
zhuo <510348414@qq.com>
2018-11-20 17:04:23 +0800

    修改头部引入 增加动画

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app" class="app">
-    <router-view/>
+    <!--header -->
+    <blogHeader />
+    <!--路由展示-->
+    <transition name="left">
+      <router-view class="app-view"></router-view>
+    </transition>
   </div>
 </template>
 
-<script>
+<script>import blogHeader from './components/blog-header/blogHeader'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {blogHeader}
 }
 </script>
 
@@ -16,5 +22,30 @@ export default {
     height: 100%;
     margin: 0;
     padding: 0;
+  }
+  .app-view{
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    position:absolute;
+  }
+  .left-enter{
+    transform: translateX(100%);
+  }
+  .left-enter-to{
+    transform: translateX(0);
+  }
+  .left-enter-active{
+    transition: 0.5s;
+  }
+  .left-leave{
+    transform: translateX(0);
+  }
+  .left-leave-to{
+    transform: translateX(-100%);
+  }
+  .left-leave-active{
+    transition: 0.5s;
   }
 </style>
