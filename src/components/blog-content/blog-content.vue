a647c2e19fc3068614127b813e8d276f7bc4fea9
zhuo <510348414@qq.com>
2018-12-14 17:57:34 +0800

    新增了公告 以及3个列表

--- src/components/blog-content/blog-content.vue
+++ src/components/blog-content/blog-content.vue
@@ -2,26 +2,30 @@
   <!--博客主体-->
   <main>
     <div class="blog-content">
+      <!--选项卡卡片-->
+      <blogSider />
+      <!--新文章-->
+      <blogNewList />
       <!--文章部分-->
       <blogArticle />
-      <!--右边卡片-->
-      <blogSider />
     </div>
   </main>
 </template>
 
 <script>
+// import blogNewList from '../blog-newList/blog-newList'
 import blogArticle from '../blog-article/blog-article'
 import blogSider from '../blog-sider/blogSider'
+import blogNewList from  '../blog-newList/blog-newList'
 export default {
   name: 'blog-content',
-  components: {blogArticle, blogSider}
+  components: {blogArticle, blogSider,blogNewList}
 }
 </script>
 
 <style scoped>
   .blog-content{
-    width: 1000px;
+    width: 695px;
     margin: 40px auto 60px;
     overflow: hidden;
     clear: both;
