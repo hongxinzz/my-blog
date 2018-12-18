3cb6a5e453661628c8a67d178423d780959f8ea5
zhuo <510348414@qq.com>
2018-12-18 15:20:47 +0800

    每一次路由都回到顶部

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import BlogAdmin from '../components/blog-admin/blog-admin'
 import BlogIndex from '../components/blog-index/blog-index'
 import bloogDetails from '../components/blog-details/blog-details'
 import blogTags from '../components/blog-tags/blogTags'
+import BlogMessage from '../components/blog-message/blog-message'
 
 Vue.use(Router)
 
@@ -28,6 +29,25 @@ export default new Router({
       path: '/tags/:type',
       name: 'tags',
       component: blogTags
+    },
+    {
+      path:'/message',
+      name: 'message',
+      component:BlogMessage
     }
-  ]
+  ],
+  /**
+   *  每一次点击页面回到顶部
+   * @param to
+   * @param from
+   * @param savedPosition
+   * @returns {*}
+   */
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
 })
