0e3ed4d41026f21635bf12977d2037c641a5c2e3
zhuo <510348414@qq.com>
2018-12-24 11:23:47 +0800

    修改文件引入方式

--- src/main.js
+++ src/main.js
@@ -1,22 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
+// import Vue from 'vue'
 import App from './App'
 import router from './router'
-import axios from 'axios'
-import iView from 'iview'
 import store from './store/store.js'
-import 'iview/dist/styles/iview.css'
 import Highlight from './plugins/highlight'
 import 'highlight.js/styles/atom-one-dark-reasonable.css'
-import  './assets/css/iconfont.css'
+const Vue = require('vue')
+const axios = require('axios')
 
 Vue.config.productionTip = false
-Vue.use(iView)
-// Vue.use(axios);
 Vue.prototype.axios = axios
-
 Vue.use(Highlight)
+
+
 
 /* eslint-disable no-new */
 new Vue({
