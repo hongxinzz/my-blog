ba8683ad571e2ebafad27d0e7978e11c46dfd4db
zhuo <510348414@qq.com>
2019-01-11 22:34:49 +0800

    新增获取所有标签 修改返回值

--- server/controller/article.js
+++ server/controller/article.js
@@ -1,19 +1,54 @@
 const articleModule = require('../module/postArticle');
 
-exports.findArticleBytag = async ctx =>{
+/**
+ * 通过标签查找文章(模糊搜索)
+ * @param ctx
+ */
+module.exports.findArticleByTag = async ctx =>{
   let data = ctx.query
-  await articleModule.findArticleBytag(data.tags)
+  await articleModule.findArticleByTag(data.tags)
     .then(res=>{
-      ctx.body = res;
-    }).catch((err)=>{
-      ctx.body = err
+      checkDataType(ctx,res)
+    })
+}
+
+/**
+ * 文章详情页
+ * @param ctx
+ */
+module.exports.findArticleOne = async ctx =>{
+  let data = ctx.query
+  await articleModule.findArticleOne(data.id)
+    .then(res=>{
+      checkDataType(ctx,res)
+    })
+}
+
+/**
+ * 获取所有标签
+ * @param ctx
+ */
+module.exports.findArticleAllTag = async ctx =>{
+  await articleModule.findArticleAllTag()
+    .then(res=>{
+      let tagList = []
+      res.forEach(data=>{
+        data.tags.forEach(tag=>{
+          tagList.push(tag)
+        })
+      })
+      tagList = Array.from(new Set(tagList))
+      checkDataType(ctx,tagList)
     })
 }
 
 
-// function returnJSON (ctx,data) {
-//   // 设置状态码为200
-//   ctx.status = 200
-//   //数据结果
-//   ctx.body = data;
-// }
+
+function checkDataType(ctx,data){
+  if(data){
+    ctx.body = data;
+  }else{
+    ctx.body = 'error'
+  }
+
+}
