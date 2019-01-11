ec9e7035a611ae9ddda689385c9201cffab00e12
zhuo <510348414@qq.com>
2019-01-11 22:37:05 +0800

    修改路由逻辑

--- server/routers/article.js
+++ server/routers/article.js
@@ -1,8 +1,17 @@
 const router = require('koa-router')();
-
+//引入文章数据模型
 const article = require('../controller/article');
-// 读取blog 的标签对应文章
-router.get('/api/get_blogs_by_tags', article.findArticleBytag)
+
+// 读取标签对应文章(模糊搜索)
+router.get('/api/get_blogs_by_tags', article.findArticleByTag)
+
+// 读取文章详情页面
+router.get('/api/get_blogs_one', article.findArticleOne)
+
+// 读取所有文章的标签
+router.get('/api/get_blogs_tags', article.findArticleAllTag)
+
+
 
 
 
