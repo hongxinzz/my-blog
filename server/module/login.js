530f8edfe73b4dfa36bc8735ef56687623b505db
zhuo <510348414@qq.com>
2019-01-12 15:12:38 +0800

    删除多余数据模型和原生node

--- server/module/login.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-// 登录模板
-const user_login = new mongoose.Schema({
-  userName: String,
-  password: String
-})
-
-
-module.exports = mongoose.model('UserLogin', user_login)
