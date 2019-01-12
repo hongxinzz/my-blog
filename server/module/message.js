1fe0e7b55759f34ac9b524ceccacdad14f0957f9
zhuo <510348414@qq.com>
2019-01-12 13:45:49 +0800

    完成留言面板 逻辑

--- server/module/message.js
+++ server/module/message.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 
-// 发布文章模板
-const post_message = new mongoose.Schema({
+/**
+ * 发布留言模板(游客模式)
+ * @type {mongoose.Schema}
+ */
+const message = new mongoose.Schema({
   id: Number,
   userName:String,
   email:String,
@@ -9,4 +12,35 @@ const post_message = new mongoose.Schema({
   pic:String
 })
 
-module.exports = mongoose.model('PostMessage', post_message)
+let _Message = mongoose.model('PostMessage', message)
+
+/**
+ * 保存留言
+ * @param data
+ */
+module.exports.postMessage = (data) =>{
+  let person = new _Message({
+    userName: data.userName,
+    email: data.password,
+    content:data.content,
+    pic:data.pic
+  })
+  return person.save();
+}
+
+/**
+ * 获取留言
+ * @returns {RegExpExecArray | * | null}
+ */
+module.exports.getMessage = () =>{
+  return _Message.find({}).sort({_id:-1}).exec()
+}
+
+/**
+ * 删除留言
+ * @param id
+ */
+module.exports.deleteMessage = (id) =>{
+  console.log(id)
+  return _Message.remove({_id:id}).exec();
+}
