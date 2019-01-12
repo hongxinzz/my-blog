d3b745b9bf8c394b59157b893e4b338462aac06f
zhuo <510348414@qq.com>
2019-01-12 15:29:49 +0800

    新 koa 后台

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
-// 引入 http
-const http = require('http')
-const url = require('url')
-const mongoose = require('mongoose')
+const Koa = require('koa');
+const Router = require('koa-router');
+const mongoose = require('mongoose');
+const bodyParser = require('koa-bodyparser');
+
+const app = new Koa();
 
 // 创建一个主机名
 const hostname = '127.0.0.1'
@@ -9,12 +11,16 @@ const hostname = '127.0.0.1'
 const port = 8080
 // 连接数据库 本地需要启动
 mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true })
-// 创建一个请求头 req为http请求res为http响应x
-const server = http.createServer((req, res) => {
-  let pathname = url.parse(req.url).pathname
-  require('./router').router(req, res)
-})
-// 设置监听
-server.listen(port, hostname, () => {
+
+//使用bodyParser获取body(post方法)
+app.use(bodyParser());
+//引入数据库模型 并通过路由返回数据
+app.use(require('./routers/article-router.js').routes())
+app.use(require('./routers/message-router.js').routes())
+app.use(require('./routers/user-router.js').routes())
+
+
+app.listen(port, hostname, () => {
   console.log(`server running at http://${hostname}:${port}/`)
 })
+
