985596dcc6152502f1c8a2492d9ad6a1378dc515
zhuo <510348414@qq.com>
2019-01-12 15:39:11 +0800

    去除elementUI 修改一些逻辑

--- src/components/blog-admin/blog-admin.vue
+++ src/components/blog-admin/blog-admin.vue
@@ -2,27 +2,22 @@
       <div class="blog-admin-login">
         <div class="from-wrap">
           <h3>My Bolg</h3>
-          <Form ref="formInline" >
-            <FormItem prop="user">
-              <Input type="text" v-model="formInline.userName" placeholder="请输入账号">
-              <Icon type="ios-person-outline" slot="prepend"></Icon>
-              </Input>
-            </FormItem>
-            <FormItem prop="password">
-              <Input type="password" v-model="formInline.password" placeholder="请输入密码">
-              <Icon type="ios-lock-outline" slot="prepend"></Icon>
-              </Input>
-            </FormItem>
-            <FormItem>
-              <Button type="primary" @click="handleSubmit('formInline')">Signin</Button>
-            </FormItem>
+          <Form >
+            <label>
+              <input type="text" v-model="formInline.userName" placeholder="请输入账号"  />
+            </label>
+            <label>
+              <input type="password" v-model="formInline.password" placeholder="请输入密码" />
+            </label>
+            <label>
+              <Button type="primary" @click="handleSubmit()">登录</Button>
+            </label>
           </Form>
         </div>
       </div>
 </template>
 
 <script>
-// import baseApi from '../../shared/baseApi.js'
 export default {
   name: 'blog-admin',
   data () {
@@ -30,45 +25,19 @@ export default {
       formInline: {
         userName: '',
         password: ''
-      },
-      ruleInline: {
-        user: [
-          { required: true, message: '请输入正确的账号', trigger: 'blur' }
-        ],
-        password: [
-          { required: true, message: '请输入正确的密码.', trigger: 'blur' },
-          { type: 'string', min: 6, message: '密码最少为6位', trigger: 'blur' }
-        ]
       }
     }
   },
   methods: {
-    handleSubmit (name) {
-      let parma = {
+    handleSubmit () {
+      this.axios.post('/api/login', {
         userName: this.formInline.userName,
         password: this.formInline.password
-      }
-      this.$refs[name].validate((valid) => {
-        if (valid) {
-          console.log(this.formInline.userName,this.formInline.password)
-          this.axios.post('/api/login', {
-            userName: this.formInline.userName,
-            password: this.formInline.password
-          }).then(function (data) {
-            console.log(data)
-            window.localStorage.setItem('USER_LOGIN','true');
-          })
-            .catch(function (error) {
-              console.log(error)
-            })
-          // this.$Message.success('Success!')
-        } else {
-          // this.$Message.error('Fail!')
-        }
+      }).then(function (data) {
+        console.log(data)
+        if(data.data === "error") return
+        window.localStorage.setItem('USER_LOGIN','true');
       })
-      // this.baseApi.get('/api/login', parma).then(data => {
-      //   console.log(data)
-      // })
     }
   }
 }
