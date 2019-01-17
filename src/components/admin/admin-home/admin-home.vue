488843fa16627f7e8f038fa91bbe66414520b651
zhuo <510348414@qq.com>
2019-01-17 21:55:31 +0800

    后台首页

---
+++ src/components/admin/admin-home/admin-home.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="admin-home">
+        <div class="slider">
+            <AdminSlider />
+        </div>
+        <div class="child-router">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+    import  AdminSlider from '../admin-slider/admin-slider.vue'
+    export default {
+        name: "admin-home",
+        components:{AdminSlider}
+    }
+</script>
+
+<style scoped lang="scss">
+.admin-home{
+    display: flex;
+    .slider{
+        width: 260px;
+        z-index: 99999;
+    }
+    .child-router{
+        flex: 1;
+        padding-top: 58px;
+        background: #001529;
+    }
+}
+</style>
