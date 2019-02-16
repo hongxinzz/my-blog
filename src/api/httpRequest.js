a3c74ff65fcb17b57f31722c9b8f511b96cf5a63
zhuo <510348414@qq.com>
2019-02-16 21:15:45 +0800

    封装axios请求

---
+++ src/api/httpRequest.js
@@ -0,0 +1,33 @@
+/**
+ * get方法，对应get请求
+ * @param {String} url
+ * @param {Object} params
+ */
+export function get(url, params) {
+    return new Promise((resolve, reject) => {
+        axios.get(url, {
+            params: params
+        }).then(res => {
+            resolve(res.data);
+        }).catch(err => {
+            reject(err.data)
+        })
+    });
+}
+
+/**
+ * post方法，对应post请求
+ * @param {String} url
+ * @param {Object} params
+ */
+export function post(url, params) {
+    return new Promise((resolve, reject) => {
+        axios.post(url, params)
+            .then(res => {
+                resolve(res.data);
+            })
+            .catch(err => {
+                reject(err.data)
+            })
+    });
+}
