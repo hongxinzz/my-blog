4b0fbde98f15697f2e152d4b4d6a472fb1f587d8
zhuo <510348414@qq.com>
2019-03-07 20:43:10 +0800

    修改状态

--- server/token/token.js
+++ server/token/token.js
@@ -4,28 +4,32 @@ const user = require('../module/user')
 
 module.exports.check = async ctx => {
     if(ctx.request.header.authorization){
-      return  jwt.verify(ctx.request.header.authorization.split(' ')[1], 'token', (error, data) => {
+        console.log(ctx.request.header.authorization)
+      return  jwt.verify(ctx.request.header.authorization.split(' ')[1], 'token', (error,data ) => {
             if (error && error.name === "TokenExpiredError") {
+                ctx.status = 401;
                 ctx.body={
-                    code:401,
                     errMessage:'token过期，重新登录'
                 };
                 return
             }
+            console.log(data)
             let userName = {userName:data.user};
             return user.queryRegister(userName).then( async res=>{
                 if (res) {
                     console.log(1)
                    return true;
                 } else {
+                    ctx.status = 401;
                     ctx.body={
                         code:'error',
-                        errMessage:'用户名不存在'
+                        errMessage:'token无效'
                     };
                 }
             });
         })
     }else{
+        ctx.status = 401;
         ctx.body = {
             code:401,
             message: '没有token'
