f49463cf2fc9718aa2b78fc598bf8a372aedae66
zhuo <510348414@qq.com>
2019-03-07 20:46:58 +0800

    音乐首页

---
+++ src/components/views/blog-music/blog-music.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="music-wrap">
+        <div class="music">
+            <MusicTab></MusicTab>
+           <div class="music-content">
+               <router-view></router-view>
+           </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import MusicTab from './blog-music-tab/blog-music-tab.vue'
+    export default {
+        name: "blog-music",
+        components: {MusicTab},
+    }
+</script>
+
+<style scoped lang="scss">
+    .music-wrap{
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        background: linear-gradient(to top, #bdc3c7, #2c3e50);
+        .music{
+            position: relative;
+            width: 1460px;
+            margin:  0 auto;
+            display: flex;
+            background: #fff;
+            .music-tab{
+            border-right: 2px solid #f2f2f2;
+            }
+            .music-content{
+                flex: 1;
+                overflow: hidden;
+                max-width: 1200px;
+                height: 780px;
+            }
+        }
+    }
+
+</style>
