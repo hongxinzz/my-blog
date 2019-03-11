4f1d1fa27015d56cf88c6ce645e62a677f638fd7
zhuo <510348414@qq.com>
2019-03-11 18:08:49 +0800

    获取歌曲列表

--- src/api/api.js
+++ src/api/api.js
@@ -30,7 +30,24 @@ export  const getSingerList = () =>{
         hostUin: 0,
         needNewCode: 0,
         platform: 'yqq'
-    })
+    });
 
     return jsonp(url, data, option)
-}
+};
+
+export  const getSingerDetail = (id) =>{
+    const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg'
+    const data = Object.assign({},commonParams,{
+        hostUin:0,
+        needNewCode:0,
+        platform:'yqq',
+        order:'listen',
+        begin:0,
+        num:100,
+        songstatus:1,
+        g_tk:1664029744,
+        singermid:id,
+    });
+
+    return jsonp(url, data, option)
+};
