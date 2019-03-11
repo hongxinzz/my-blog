8a4f98ddfba513a89304f7d396862bf7ce950b8e
zhuo <510348414@qq.com>
2019-03-11 18:20:02 +0800

    歌手详情页面

---
+++ src/components/views/blog-music/blog-singer-detail/blog-singer-detail.vue
@@ -0,0 +1,33 @@
+<template>
+
+</template>
+
+<script>
+    import {mapGetters}  from 'vuex'
+    import {getSingerDetail} from '../../../../../src/api/api'
+    export default {
+        name: "blog-singer-detail",
+        computed:{
+            ...mapGetters([
+                'singer'
+            ])
+        },
+        created(){
+            console.log(this.singer.id)
+            this._getDetail()
+        },
+        methods:{
+            _getDetail(){
+                getSingerDetail(this.singer.id).then(data=>{
+                    if(data.code  === 0){
+                        console.log(data.data.list)
+                    }
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
