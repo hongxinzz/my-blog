9eae97bf5b444ae1e8bb4a78577e138dbf487477
zhuo <510348414@qq.com>
2019-03-12 17:58:38 +0800

    新增获取歌曲播放url

--- src/api/api.js
+++ src/api/api.js
@@ -51,3 +51,40 @@ export  const getSingerDetail = (id) =>{
 
     return jsonp(url, data, option)
 };
+
+export function getSongUrl(songmid) {
+    const url = 'https://u.y.qq.com/cgi-bin/musicu.fcg'
+
+    const data = Object.assign({}, commonParams, {
+        loginUin: 0,
+        hostUin: 0,
+        platform: 'yqq',
+        needNewCode: 0,
+        data: getData(songmid)
+    })
+
+    return jsonp(url, data, {})
+}
+function getData(songmid) {
+    let data = {
+        req: {
+            module: 'CDN.SrfCdnDispatchServer',
+            method: 'GetCdnDispatch',
+            param: { guid: '3102513652', calltype: 0, userip: '' }
+        },
+        req_0: {
+            module: 'vkey.GetVkeyServer',
+            method: 'CgiGetVkey',
+            param: {
+                guid: '3102513652',
+                songmid: [songmid],
+                songtype: [0],
+                uin: '0',
+                loginflag: 1,
+                platform: '20'
+            }
+        },
+        comm: { uin: 0, format: 'json', ct: 20, cv: 0 }
+    }
+    return JSON.stringify(data)
+}
